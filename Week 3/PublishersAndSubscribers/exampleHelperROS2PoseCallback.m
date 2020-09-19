db3e773691c1eeb44779b165ae804c85aab31bec
Adi Pasic <35309039+ajpasic@users.noreply.github.com>
2020-09-19 13:56:44 -0500

    Add files via upload

---
+++ Week 3/PublishersAndSubscribers/exampleHelperROS2PoseCallback.m
@@ -0,0 +1,19 @@
+function exampleHelperROS2PoseCallback(message)
+    %exampleHelperROS2PoseCallback Subscriber callback function for pose data    
+    %   exampleHelperROS2PoseCallback(MESSAGE) returns no arguments - it instead sets 
+    %   global variables to the values of position and orientation that are
+    %   received in the ROS 2 message MESSAGE.
+    %   
+    %   See also ROSPublishAndSubscribeExample.
+    
+    %   Copyright 2019 The MathWorks, Inc.
+    
+    % Declare global variables to store position and orientation
+    global pos
+    global orient
+    
+    % Extract position and orientation from the ROS message and assign the
+    % data to the global variables.
+    pos = [message.linear.x message.linear.y message.linear.z];
+    orient = [message.angular.x message.angular.y message.angular.z];
+end
